e4f4ad4d828e83ab038bdf17511b62af20ce1843
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-07 12:44:23 +0100

    Removed score list if no scores are present

--- Menues/scoreList.gd
+++ Menues/scoreList.gd
@@ -4,12 +4,21 @@ var scoreList : Array[float]
 @export var numberOfScores : int = 10
 #Only to include test data
 
+func _ready() -> void:
+	var lable = $"../ScoreBoardLable"
+	hide()
+	lable.hide()
+
 func addScore(score : float):
 	scoreList.append(score)
 	scoreList.sort_custom(dessending)
 	
 func updateScores():
 	clear()
+	if(len(scoreList)>=1):
+		var lable = $"../ScoreBoardLable"
+		show()
+		lable.show()
 	for i in range(len(scoreList)):
 		add_item(str(int(scoreList[i])))
 		if(i+1>=numberOfScores):
