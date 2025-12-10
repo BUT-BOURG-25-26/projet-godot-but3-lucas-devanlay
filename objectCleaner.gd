6c824c63bd83193cd0331d32fdf4874ac2070b75
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-10 22:03:20 +0100

    cleaned objectCleaner's code

--- objectCleaner.gd
+++ objectCleaner.gd
@@ -10,9 +10,4 @@ func _process(delta: float) -> void:
 
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if(body is BaseBox or body is Spike ):
-		print(body.name)
 		body.queue_free()
-
-func _on_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
-	print(area.name)
-	area.queue_free()
