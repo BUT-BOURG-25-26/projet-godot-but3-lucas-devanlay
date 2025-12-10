1da479f3e517a5bdec97237def662f7a15ba6922
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-10 22:03:59 +0100

    If a strawbery is in another element it will now rise up until being above it

--- worldElements/bonus/strawberry.gd
+++ worldElements/bonus/strawberry.gd
@@ -17,10 +17,6 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		gameManager.addToExternalScore(value)
 		hide()
 		value= 0
-
-func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
-	if(body is Player):
-		var gameManager = get_tree().get_first_node_in_group("gameManager")
-		gameManager.addToExternalScore(value)
-		hide()
-		value= 0
+	if(body is Spike or body is BaseBox):
+		print(name, " moved up")
+		global_position.y+=1
