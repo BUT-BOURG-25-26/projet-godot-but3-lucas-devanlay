b1868b67b8e8c16adf109a5e05bdbd2df4c63fd4
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-11 20:18:47 +0100

    Added jump sound effect

--- player/player.gd
+++ player/player.gd
@@ -13,6 +13,7 @@ var dashing : bool = false
 var model : PlayerModel
 var respawnSFX : AudioStreamPlayer
 var dashSFX : AudioStreamPlayer
+var jumpSFX : AudioStreamPlayer
 
 func _ready() -> void:
 	gameManager = get_tree().get_first_node_in_group("gameManager")
@@ -20,6 +21,7 @@ func _ready() -> void:
 	model = $playerModel
 	respawnSFX= $respawn
 	dashSFX = $dash
+	jumpSFX = $jump
 	model.setHairToRed()
 	
 func _physics_process(delta: float) -> void:
@@ -31,7 +33,9 @@ func _physics_process(delta: float) -> void:
 		elif(!is_on_floor()):
 			velocity.y -= 2
 		if(is_on_floor()):
-			velocity.y +=  getInputUp()*40
+			velocity.y += getInputUp()*40
+			if(velocity.y>5):
+				jumpSFX.play()
 		velocity.x = (-(getInputLeft()) + (getInputRight()))*20
 		if(canDash && Input.get_action_strength("dash")):
 			dash()
