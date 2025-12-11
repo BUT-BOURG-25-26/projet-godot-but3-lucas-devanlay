836fd49c3f77ff9f44d0af1b6ae795cd92ddb8b2
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-11 17:34:28 +0100

    Added Abstract Bonus class, and made Strawberry a child of Bonus

---
+++ worldElements/bonus/bonus.gd
@@ -0,0 +1,23 @@
+class_name Bonus
+extends Node3D
+
+var value : float = 0
+var animationPlayer : AnimationPlayer
+var animationToPlay : String  = ""
+ 
+func _ready() -> void:
+	animationPlayer = $AnimationPlayer
+	
+func _process(delta: float) -> void:
+		if(!animationPlayer.is_playing() and animationToPlay!=""):
+			animationPlayer.play(animationToPlay)
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if(body is Player):
+		var gameManager = get_tree().get_first_node_in_group("gameManager")
+		gameManager.addToExternalScore(value)
+		hide()
+		value= 0
+	if(body is Spike or body is BaseBox):
+		print(name, " moved up")
+		global_position.y+=1
--- worldElements/bonus/strawberry.gd
+++ worldElements/bonus/strawberry.gd
@@ -1,22 +1,7 @@
 class_name Strawberry
-extends Node3D
+extends Bonus
 
-var value : float = 500
-var animationPlayer : AnimationPlayer
 func _ready() -> void:
-	animationPlayer = $AnimationPlayer
-	animationPlayer.play("rotation")
-	
-func _process(delta: float) -> void:
-		if(!animationPlayer.is_playing()):
-			animationPlayer.play("rotation")
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	if(body is Player):
-		var gameManager = get_tree().get_first_node_in_group("gameManager")
-		gameManager.addToExternalScore(value)
-		hide()
-		value= 0
-	if(body is Spike or body is BaseBox):
-		print(name, " moved up")
-		global_position.y+=1
+	super()
+	animationToPlay = "rotation"
+	value = 500
